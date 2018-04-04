edcce8936036d537ba31b81e18bee24212ac7275
kristinschramm <kristin.schramm@gmail.com>
2018-04-04 16:52:52 -0500

    overtime update

---
+++ PaycheckCalculator/payCalc.js
@@ -0,0 +1,12 @@
+let hoursWorked= document.forms["userPayinfo"]["hoursWorked"].value;
+
+console.log(hoursWorked);
+
+/**let ratePay;
+let grossPay;
+
+function payCalculator(){
+	
+	
+}
+document.getElementByID("total").internal = myFunction*/
--- paycheckCalculator.js
+++ paycheckCalculator.js
@@ -4,6 +4,9 @@ let ratePay= parseFloat(prompt("What is your hourly rate?"), 2);
 console.log(hoursWorked);
 console.log(ratePay);
 
+if (hoursWorked >= 40) {
+ratePay= ratePay*1.5}
+
 let paycheck= (hoursWorked * ratePay);
 
 console.log(paycheck);
