2ada4d09708a2a684985404076916222def0d38e
kristinschramm <kristin.schramm@gmail.com>
2018-04-06 07:34:24 -0500

    create reverseString

---
+++ PaycheckCalculator/reverseString.js
@@ -0,0 +1,10 @@
+
+let userInputString=document.getElementById("uIString").value;
+
+
+reverseString(){
+
+	let userInputString=document.getElementById("uIString").value;
+	console.log(userInputString.length);
+
+}
